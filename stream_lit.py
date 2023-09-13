066502d3b820987a2ffbd8a9579643c34e75bc29
Iqra Anwar <57810189+IqraBaluch@users.noreply.github.com>
2023-09-13 13:17:06 +0500

    Update stream_lit.py
    added more items into menu

--- stream_lit.py
+++ stream_lit.py
@@ -3,3 +3,5 @@ import streamlit
 streamlit.title("Healthy Food")
 streamlit.header("Menu For Breackfast")
 streamlit.text('1. Orange juice, brown bread and doodh patti')
+streamlit.text('2. apple slices, boiled egg and doodh patti')
+streamlit.text('3. milk shake, half fried egg and doodh patti')
