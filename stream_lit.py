4e588b7b27868602109dd49e851eff3f6f5a4f8d
Iqra Anwar <57810189+IqraBaluch@users.noreply.github.com>
2023-09-13 13:21:21 +0500

    Update stream_lit.py
    added another header

--- stream_lit.py
+++ stream_lit.py
@@ -11,3 +11,5 @@ streamlit.header(" 🥑🍞Some other items")
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
