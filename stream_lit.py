32059e0a886f262ad5ff6bd16f69f62051157f7e
Iqra Anwar <57810189+IqraBaluch@users.noreply.github.com>
2023-09-13 13:51:04 +0500

    Update stream_lit.py

--- stream_lit.py
+++ stream_lit.py
@@ -12,7 +12,7 @@ streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🥑🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
