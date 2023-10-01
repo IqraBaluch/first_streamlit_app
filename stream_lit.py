82665df2f83401f702b292173dc74bddf5bd35fd
Iqra Anwar <57810189+IqraBaluch@users.noreply.github.com>
2023-10-01 21:17:44 +0500

    Update stream_lit.py

--- stream_lit.py
+++ stream_lit.py
@@ -1,7 +1,7 @@
 
-
-import streamlit
 import snowflake.connector
+import streamlit
+
 streamlit.title("Healthy Food")
 streamlit.header("Menu For Breackfast")
 streamlit.text('1. Orange juice, brown bread and doodh patti')
