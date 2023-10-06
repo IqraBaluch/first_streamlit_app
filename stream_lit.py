8cdfc5b4925771cb6a76c9afe01b97506218476b
Iqra Anwar <57810189+IqraBaluch@users.noreply.github.com>
2023-10-06 12:03:29 +0500

    Update stream_lit.py

--- stream_lit.py
+++ stream_lit.py
@@ -6,11 +6,10 @@ import streamlit
 
 streamlit.title('Snowflake Application')
 
-
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🥣Breakfast Menu🍞')
+streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
+streamlit.text('  🐔Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑Hard-Boiled Free-Range Egg')
 
 
 
