6d170419f500b1428f5cf7bf85e1c8cb1cf2d9da
Iqra Anwar <57810189+IqraBaluch@users.noreply.github.com>
2023-10-06 14:19:54 +0500

    Update stream_lit.py

--- stream_lit.py
+++ stream_lit.py
@@ -1,41 +1,37 @@
+
 import requests
 import streamlit
-import pandas
+import pandas as pd
 import snowflake.connector
 from urllib.error import URLError
 
 
 
-
 streamlit.title('Snowflake Application')
 
-streamlit.header('🥣Breakfast Menu🍞')
-streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
-streamlit.text('  🐔Kale, Spinach & Rocket Smoothie')
-streamlit.text('🥑Hard-Boiled Free-Range Egg')
 
+#
+# streamlit.header('Breakfast Menu') #
+# streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+# streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+# streamlit.text('🐔 🥑🍞 Hard-Boiled Free-Range Egg')
+#
+df = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+# # Let's put a pick list here so they can pick the fruit they want to include
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-#accesing fruits names from a txt file
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-## add a user interactive widget called a Multi-select that will allow users to pick the fruits they want in their smoothies.
-# Let's put a pick list here so they can pick the fruit they want to include 
-my_fruit_list = my_fruit_list.set_index('Fruit')
+df = df.set_index('Fruit')
+# streamlit.multiselect("Pick some fruits:", list(df.index),['Avocado','Strawberries'])
+# streamlit.dataframe(df)
 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-streamlit.dataframe(fruits_to_show)
+def my_fun(fruit):
+    res = requests.get("https://fruityvice.com/api/fruit/"+ fruit)
+    fruityvice_normalized = pd.json_normalize(res.json())
+    return fruityvice_normalized
 
+selected = streamlit.multiselect("Pick some fruits:", list(df.index),['Avocado','Strawberries'])
+show = df.loc[selected]
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from FRUIT_LOAD_LIST")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
-
-
+streamlit.dataframe(show)
 
 
 #new section to display
@@ -50,11 +46,50 @@ try:
         streamlit.dataframe(responce_back)
 except URLError as e:
     streamlit.error()
+
+
 streamlit.stop() #do not run anything past here.
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header("The list contain:")
+streamlit.dataframe(my_data_rows)
+input = streamlit.text_input('What fruit u would like to add in database')
+streamlit.write(f'you entered this fruit name : "{input}", Are you sure you want to add this?')
+import streamlit as st
+
+def my_fun_no():
+    st.write('Your record has not been added')
+def my_fun_yes():
+    st.write(f'Thanks for Adding: {input}')
+
+# Use a lambda function to pass my_fun as the on_click callback
+# No = st.button('No', on_click=lambda: my_fun(), type="secondary")
+No = st.button('No', type="secondary")
+Yes = st.button('Yes', type="secondary")
+if No == True:
+    my_fun_no()
+elif Yes == True:
+    my_fun_yes()
+    my_cur.execute(f"INSERT INTO PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST VALUES ('{input}')")
+    streamlit.header("The updated list now:")
+    my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+    my_data_rows = my_cur.fetchall()
+    st.dataframe(my_data_rows)
 
 
 
+# my_cur.execute(f'insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values {input}'
+# Create a parameterized query with placeholders
+# sql = 'INSERT INTO PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST VALUES (?)'
+
+# Execute the query with the input values
+
+
+# my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+# streamlit.dataframe(my_data_rows)
 
 
 
