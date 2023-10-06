c08293b8f9ad3b9c6e50097c5da21fb7f8a78541
Iqra Anwar <57810189+IqraBaluch@users.noreply.github.com>
2023-10-06 12:10:22 +0500

    Update stream_lit.py

--- stream_lit.py
+++ stream_lit.py
@@ -15,8 +15,13 @@ streamlit.text('🥑Hard-Boiled Free-Range Egg')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+#accesing fruits names from a txt file
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+## add a user interactive widget called a Multi-select that will allow users to pick the fruits they want in their smoothies.
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
+
 
 
 
